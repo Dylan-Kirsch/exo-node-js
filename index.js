39228b7a8e08ec40c7be091c319283fd1e151a07
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2023-03-23 12:47:22 +0100

    exo..

--- index.js
+++ index.js
@@ -4,26 +4,23 @@ const users = require('./users.json')
 
 app.use(express.json())
 
-app.get('/users', (req, res) => 
-{
+app.get('/users', (req, res) => {
     res.status(200).json(users)
 })
 
-app.get('/users/:id', (req, res) => 
-{
+app.get('/users/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    const users = users.find(user => user.id === id)
-    res.status(200).json(users)
+    const user = user.find(user => user.id === id)
+    res.status(200).json(user)
 })
 
-app.post('/users', (req, res) => 
-{
+app.post('/users', (req, res) => {
     users.push(req.body)
     res.status(200).json(users)
 })
 
-app.listen(8181, () => 
-{ 
+
+app.listen(8080, () => { 
     console.log("server go") 
 })
 
