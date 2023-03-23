518caf9dcbb8577c86bf3f2301ca7d4fff92f4d1
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2023-03-23 16:15:11 +0100

    exo...

--- index.js
+++ index.js
@@ -10,7 +10,22 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    const users = users.find(user => user.id === id)
+    const user = users.find(user => user.id === id)
+    res.status(200).json(user)
+})
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = users.find(user => user.id === id)
+    users.splice(users.indexOf(user),1)
+    res.status(200).json(users)
+})
+
+app.put('users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    let user = users.find(user => user.id === id)
+    user.user = req.body.user,
+    user.mdp = req.body.mdp,
     res.status(200).json(users)
 })
 
